3ba8b11dc62a4c7ff98116487641b45c500c85e3
Stewart McKinlay <stewart.mckinlay@gmail.com>
2021-03-16 16:33:55 -0600

    Update ControlFlow.java

--- src/java_basics/ControlFlow.java
+++ src/java_basics/ControlFlow.java
@@ -42,6 +42,26 @@ public class ControlFlow {
 		
 		System.out.println(opinion);
 		
+		
+		int month = 2;
+		String monthString;
+		
+		switch(month) {
+		case 1: 
+			monthString = "January";
+			break;
+		case 2: 
+			monthString = "February";
+			break;
+		case 3: 
+			monthString = "March";
+			break;
+		default: 
+			monthString = "Invalid Input";
+		}
+		
+		System.out.println(monthString);
+		
 	}
 
 }
